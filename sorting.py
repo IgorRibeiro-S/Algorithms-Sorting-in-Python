90b9e7d069d541b666027aaba47c20dfc1a08e75
Igor Ribeiro <igor_rsilva97@hotmail.com>
2022-04-23 23:57:51 -0300

    Create Algorithm Shell Sort, Insertion Sort and update all algorithms

--- sorting.py
+++ sorting.py
@@ -1,27 +1,20 @@
 from random import randint
+import time
 
 
-def quicksort(lista, inicio=0, fim=None):
-    if fim is None:
-        fim = len(lista) - 1
-    if inicio < fim:
-        pivot = lista[fim]
-        indice = inicio
-        for c in range(inicio, fim):
-            if lista[c] <= pivot:
-                aux = lista[c]
-                lista[c] = lista[indice]
-                lista[indice] = aux
-                indice += 1
-        aux2 = lista[fim]
-        lista[fim] = lista[indice]
-        lista[indice] = aux2
-        quicksort(lista, inicio, fim - 1)
-        quicksort(lista, fim + 1, fim)
-        return indice
+# Insertion Sort = ordenação por inserção
+def insertsort(lista):
+    num = len(lista)
+    for c in range(1, num):
+        aux = lista[c]
+        ind = c - 1
+        while ind >= 0 and lista[ind] > aux:
+            lista[ind + 1] = lista[ind]
+            ind -= 1
+        lista[ind + 1] = aux
 
 
-# SelectionSort = ordenação por comparação
+# Selection Sort = ordenação por seleção
 def selectionsort(lista):
     num = len(lista)
     for pos in range(num - 1):
@@ -33,6 +26,23 @@ def selectionsort(lista):
             aux = lista[pos]
             lista[pos] = lista[minimo_ind]
             lista[minimo_ind] = aux
+
+
+# Shell Sort = insertion sort melhorado
+def shellsort(lista):
+    num = len(lista)
+    h = num // 2
+    while h > 0:
+        i = h
+        while i < num:
+            aux = lista[i]
+            ind = i - h
+            while ind >= 0 and lista[ind] > aux:
+                lista[ind + h] = lista[ind]
+                ind -= h
+            lista[ind + h] = aux
+            i += 1
+        h = h // 2
 
 
 # MergeSort = divisão e conquista
@@ -63,11 +73,39 @@ def mergesort(lista, inicio=0, fim=None):
                 topo_direita += 1
 
 
+# Quick Sort = ordenação por comparação
+def quicksort(lista, inicio=0, fim=None):
+    if fim is None:
+        fim = len(lista) - 1
+    if inicio < fim:
+        piv = particao(lista, inicio, fim)
+        quicksort(lista, inicio, piv - 1)
+        quicksort(lista, piv + 1, fim)
+
+
+def particao(lista, inicio, fim):
+    pivot = lista[fim]
+    indice = inicio
+    for c in range(inicio, fim):
+        if lista[c] <= pivot:
+            aux = lista[c]
+            lista[c] = lista[indice]
+            lista[indice] = aux
+            indice += 1
+    aux2 = lista[fim]
+    lista[fim] = lista[indice]
+    lista[indice] = aux2
+    return indice
+
+
 if __name__ == '__main__':
     li = []
-    for d in range(1, 7):
-        li.append(randint(1, 45))
+    for d in range(1, 100000):
+        li.append(randint(1, 100000))
 
     print(li)
+    antes = time.time()
     quicksort(li)
-    print(li)
+    depois = time.time()
+    total = (depois - antes) * 1000
+    print(f"o tempo total foi de {total:.2f} ms")
