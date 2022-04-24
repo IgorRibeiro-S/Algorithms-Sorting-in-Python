a2dacc3fe5d03a73116e2b04d923a94a5d33ebf6
Igor Ribeiro <igor_rsilva97@hotmail.com>
2022-04-23 21:44:19 -0300

    Create Algorithm quicksort

--- sorting.py
+++ sorting.py
@@ -1,10 +1,31 @@
 from random import randint
 
 
-def selection_sort(lista):
+def quicksort(lista, inicio=0, fim=None):
+    if fim is None:
+        fim = len(lista) - 1
+    if inicio < fim:
+        pivot = lista[fim]
+        indice = inicio
+        for c in range(inicio, fim):
+            if lista[c] <= pivot:
+                aux = lista[c]
+                lista[c] = lista[indice]
+                lista[indice] = aux
+                indice += 1
+        aux2 = lista[fim]
+        lista[fim] = lista[indice]
+        lista[indice] = aux2
+        quicksort(lista, inicio, fim - 1)
+        quicksort(lista, fim + 1, fim)
+        return indice
+
+
+# SelectionSort = ordenação por comparação
+def selectionsort(lista):
     num = len(lista)
     for pos in range(num - 1):
-        minimo_ind = pos  # 3
+        minimo_ind = pos
         for c in range(pos, num):
             if lista[c] < lista[minimo_ind]:
                 minimo_ind = c
@@ -14,6 +35,7 @@ def selection_sort(lista):
             lista[minimo_ind] = aux
 
 
+# MergeSort = divisão e conquista
 def mergesort(lista, inicio=0, fim=None):
     if fim is None:
         fim = len(lista)
@@ -47,5 +69,5 @@ if __name__ == '__main__':
         li.append(randint(1, 45))
 
     print(li)
-    selection_sort(li)
+    quicksort(li)
     print(li)
